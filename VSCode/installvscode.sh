e8bd62c0faf95e4f8bbbba7897d3484687e17ae5
Kuro <yoyh9ab@outlook.jp>
2019-11-21 13:28:02 +0900

    Update installvscode.sh

--- VSCode/installvscode.sh
+++ VSCode/installvscode.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget https://packagecloud.io/headmelted/codebuilds/gpgkey | sudo apt-key add
-sudo . <( wget -O - https://code.headmelted.com/installers/apt.sh )
-sudo apt-get install code-oss=1.29.0-1539702286
-sudo apt-mark hold code-oss
+wget -O - https://packagecloud.io/headmelted/codebuilds/gpgkey | apt-key add
+. <( wget -O - https://code.headmelted.com/installers/apt.sh )
+apt-get install code-oss=1.29.0-1539702286
+apt-mark hold code-oss
